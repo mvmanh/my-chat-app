8d443927b42e2a05c1acc163bcc9ef15024f1de9
Mai Van Manh <mvmanh@gmail.com>
2024-04-19 16:09:52 +0700

    fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/my-chat-app/deploy.log
 ls
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline
-ls
-echo 'pm2 reload app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 reload app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+cd /home/ec2-user/my-chat-app
+echo 'pm2 reload app' >> /home/ec2-user/my-chat-app/deploy.log
+pm2 reload app >> /home/ec2-user/my-chat-app/deploy.log
